589ba235e604c5b4e33082d44a51d506145d6fdc
Sabari VR <sabari.r@nergyindia.com>
2023-11-30 12:57:16 +0530

    Resolved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../assets/about.png";
 import Particle from "../components/Particle";
 import About from "../components/Home/About";
 import Type from "../components/Home/Type";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/portfolio", { replace: true });
+  }, []);
   return (
     <section>
       <Container fluid className="home-section" id="home">
