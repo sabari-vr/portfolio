630d5c5c07192e11cc1753a2f44b30bf96edaf1c
Sabari VR <sabari.r@nergyindia.com>
2023-11-30 16:06:47 +0530

    Resolved

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,10 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "../components/Projects/ProjectCard";
 import Particle from "../components/Particle";
 import pg from "../assets/projects/pg.png";
-import project from "../assets/projects/project.jpeg";
-import lift from "../assets/projects/lift.png";
-import krypto from "../assets/projects/krypto.png";
-import kickstart from "../assets/projects/kickstart.png";
 
 const Projects = () => {
   return (
@@ -26,8 +22,8 @@ const Projects = () => {
               isBlog={false}
               title="Lorem Ipsum"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida mauris vitae luctus iaculis. Phasellus nibh nisl, lacinia viverra maximus at, bibendum sed nibh. Sed tempus a dolor nec suscipit. Nunc eu bibendum sem, id luctus nisl. Nulla enim lectus, sollicitudin ut tristique eget, convallis eu ex. In est ligula, blandit at diam rhoncus, efficitur porta lectus. Aliquam leo nunc, laoreet a ex ac, aliquet scelerisque magna. Donec in mauris turpis. Aenean at auctor nulla. Vivamus pellentesque cursus nisi vel malesuada."
-              ghLink="https://github.com/sabari-vr"
-              demoLink="https://github.com/sabari-vr"
+              ghLink="https://github.com/sabari-vr/country_list"
+              demoLink="https://sabari-vr.github.io/country_list"
             />
           </Col>
 
