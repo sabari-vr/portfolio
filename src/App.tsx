80da26349375385a0f48b9f9b3cce824b1c20be3
Sabari V R <mailtosabarivr@gmail.com>
2026-02-21 12:32:20 +0530

    Set basename for BrowserRouter in App component and define base path in Vite config

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
